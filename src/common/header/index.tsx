20e46d6133b10689bbec7e657a469ad1bcf91be5
YidCards <info@yidcards.com>
2023-11-29 18:12:25 +0530

    Landing page first screen design with responsive

--- src/common/header/index.tsx
+++ src/common/header/index.tsx
@@ -1,17 +1,40 @@
 // create dashboard page component
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import './index.scss';
+import logowhite from "../../assets/images/logo-white.svg";
+import logoblack from "../../assets/images/logo-black.svg";
+
 
 const Header = () => {
+  
+  // Home burger menu 
+  const [isActive, setIsActive] = useState(false);
+  const toggleClass = () => {
+    setIsActive(!isActive);
+  };
+
   return (
-    <div className="container">
-      <div className="row">
-        <div className="col-12 text-center">
-          <h1>Header</h1>
-        </div>
+    <header className='header-sec'>
+      <div className="container">
+          <div className="custom-row">
+            <div className="logo-block-left">
+              <Link to="#"><img src={logowhite} alt="logo" width="322" height="77" /></Link>
+            </div>
+            <div className='menu-block-right'>
+              <div className='home-burger-menu'>
+                  <div className={isActive ? 'is-active' : '' } onClick={toggleClass}>
+                  <span className='homeburger-title'>MENU</span>
+                  <div className='homeburger-line'>
+                    <span className="line"></span>
+                    <span className="line"></span>
+                    </div>
+                  </div>
+              </div>
+            </div>
+          </div>
       </div>
-    </div>
+    </header>
   );
 };
 
--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -2,17 +2,35 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import './index.scss'
+import volumeoff from '../../assets/images/volumeoff-icon.svg';
+import volumeon from '../../assets/images/volumeon-icon.svg';
+import btnloader from '../../assets/images/btn-loader.svg';
 
 const Home = () => {
   return (
-    <div className="container">
-      <div className="row">
-        <div className="col-12 text-center">
-          <h1>Home</h1>
-          <Link to="/login">Go Login</Link>
+   <section className='main-banner'>
+     <div className="homepage-banner-video-overlay"></div>
+      <video className='connect-bg' src="https://media.giphy.com/media/klIaoXlnH9TMY/giphy.mp4" muted autoPlay loop playsInline ></video>
+      <div className='banner-content-main'>
+      <div className="container">
+        <div className="custom-row">
+          <div className="banner-content-block">
+            <span>Simplified Real Estate </span>
+            <h1>A digital way to <br/>buying and selling property</h1>
+            <Link to="/login" className='theme_btn blue_btn'>I’m Ready</Link>
+            <div className='volume-btn'>
+              <img className='volumeoff' src={volumeoff} alt="volume-off-icon" width="58" height="58" />
+              <img className='volumeon' src={volumeoff} alt="volume-off-icon" width="58" height="58" />
+              </div>
+            <div className='get-started-btn'>
+              <img src={btnloader} alt="btn-animation.svg" width="196" height="196" /> 
+              <Link to="#">Get Started</Link>
+              </div>
+          </div>
         </div>
       </div>
-    </div>
+      </div>
+   </section>
   );
 };
 
